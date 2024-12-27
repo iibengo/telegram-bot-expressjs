f3ff46777eea79e964d92d80ce709cf6fb2159f7
ion <ionilancer@gmail.com>
2024-12-27 23:22:35 +0100

    chore: search token by simbol, get metadata

--- src/web3/token/token-info/get-token-metadata.ts
+++ src/web3/token/token-info/get-token-metadata.ts
@@ -1,51 +1,45 @@
-import { Connection, PublicKey } from "@solana/web3.js";
 import { ENV, TokenListProvider } from "@solana/spl-token-registry";
-import { Metaplex } from "@metaplex-foundation/js";
 import { createTokenInfoModel, TokenInfoModel } from "../../model";
 import { isContractFormatString } from "../../functions";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {publicKey} from '@metaplex-foundation/umi-public-keys'
+import { fetchMetadata, findMetadataPda } from '@metaplex-foundation/mpl-token-metadata';
 
 export const getTokenMetadata = async (
   query: string
 ): Promise<TokenInfoModel> => {
-  const connection = new Connection("https://api.mainnet-beta.solana.com");
-
+  const umi = createUmi("https://api.mainnet-beta.solana.com");
   const tokenInfo = createTokenInfoModel();
-  const metaplex = Metaplex.make(connection);
-  let metadataAccountInfo;
 
   if (isContractFormatString(query)) {
-    const mintAddress = new PublicKey(query);
+    const mintAddress = publicKey(query);
 
-    const metadataAccount = metaplex
-      .nfts()
-      .pdas()
-      .metadata({ mint: mintAddress });
-    metadataAccountInfo = await connection.getAccountInfo(metadataAccount);
-  }
-
-  if (metadataAccountInfo) {
-    const mintAddress = new PublicKey(query);
-    const token = await metaplex
-      .nfts()
-      .findByMint({ mintAddress: mintAddress });
-    tokenInfo.name = token.name;
-    tokenInfo.symbol = token.symbol;
-    tokenInfo.logoUri = token.json?.image;
-
-    tokenInfo.address = token?.address?.toString() || "";
+    try {
+      const metadataPda = findMetadataPda(umi, { mint: mintAddress });
+      const metadata = await fetchMetadata(umi, metadataPda);
+      tokenInfo.name = metadata.name;
+      tokenInfo.symbol = metadata.symbol;
+      tokenInfo.logoUri = metadata?.uri;
+      tokenInfo.address = mintAddress.toString();
+    } catch (error) {
+      console.error("Error fetching metadata:", error);
+    }
   } else {
+    // Si el query no es una dirección válida, buscamos el symbolo la Token List
     const provider = await new TokenListProvider().resolve();
     const tokenList = provider.filterByChainId(ENV.MainnetBeta).getList();
+
     const tokenAddress = tokenList.find(
-      (token) =>
-        token.address === query
+      (token) => token.address === query
     );
-    const tokens = tokenList.filter(item=>item.symbol.toLocaleLowerCase()===query.toLocaleLowerCase()) 
-    const token =tokenAddress?tokenAddress:tokens[0];
+    const tokens = tokenList.filter(item => item.symbol.toLocaleLowerCase() === query.toLocaleLowerCase());
+    const token = tokenAddress ? tokenAddress : tokens[0];
+
     tokenInfo.name = token?.name;
     tokenInfo.symbol = token?.symbol;
     tokenInfo.logoUri = token?.logoURI;
     tokenInfo.address = token?.address || "";
   }
+
   return tokenInfo;
 };
